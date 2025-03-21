24184f4a09b8714a6f5d4793f2abb54bdecb340b
ronronn <ronaldlegaspi960@gmail.com>
2025-03-21 22:19:26 +0800

    Initial Commit

---
+++ CS1D_MTEXAM_LEGASPI/classSubjects.js
@@ -0,0 +1,7 @@
+let subjectTitle = prompt("Please enter the subject title:")
+let classSchedule = prompt("Please enter the class schedule(time,days):";
+let classroom = prompt("Please enter the classroom:");
+let classInstructor = prompt("Please enter the class instructor's name:");
+let studentName = prompt("Please enter your name:");
+
+console.log(${studentName} is currently enrolled in $ {subjectTitle} with a class schedule of ${classSchedule} at room $ {classroom}. the instructor for the subject is ${classInstructor}.);
---
+++ CS1D_MTEXAM_LEGASPI/userProfile3.js
@@ -0,0 +1,17 @@
+let userAge = pasrenInt(prompt("Please enter your age:"));
+
+if (userAge <5 ){
+  consolr.log("You are a toddler.");
+}else if (userAge>=5&& userAge <=12){
+  console.log("you are a child.");
+}else if(userAge>=13 && userAge<= 19){
+  console.log("you are a teenager.");
+}else if(userAge>= 20&& userAge <= 35{
+ console.log(you are a young adult.);
+}else if(userAge>= 36 && userAge,= 60){
+  console.log ("you are middle-aged.");
+}else if (userAge >60){
+  console.log("you are a senior.");
+}else{
+  console.log("Invalid age entered.");
+}
