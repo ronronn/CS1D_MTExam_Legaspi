669cf01936125a2e1f16c36a8841bed6681cc19a
ronronn <ronaldlegaspi960@gmail.com>
2025-03-21 17:25:43 +0800

    UserProfile.js

---
+++ UserProfile.js
@@ -0,0 +1,12 @@
+let userProfile = {};
+
+userProfile.FullName = prompt("Please enter your full name(First and Last name):");
+userProfile.age = parseInt(prompt("please enter your your age:"));
+userProfile.favoriteNumber = parseInt(prompt("please enter your favorite Number:"));
+userProfile.favoriteColor = parseInt(prompt("please enter your favorite Color:"));
+
+console.log("User Profile:");
+console.log(Full Name: ${userProfile.fullName}`);
+console.log('Age: ${userProfile.age}`);
+console.log('Favorite Color:${userProfile.favoriteColor}`);
+```
