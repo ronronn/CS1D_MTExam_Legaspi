d06a00067f0503dd05b86bac336eb84af187e44d
ronronn <ronaldlegaspi960@gmail.com>
2025-03-21 19:30:07 +0800

    CODE 6 userprofile.js

---
+++ CODE 6 userprofile.js
@@ -0,0 +1,17 @@
+let createList = prompt(" do you want to create a grocery list?(y/n");
+
+let numItems = parseInt(prompt("How many items do you want to add your  grocery list?));
+
+let grocecyList = [];
+
+let item = prompt(enter item #${i+1}:);
+groceryList.pish(item);
+}
+let groceryList.slice().sort();
+
+let groceryList.slice().reverse();
+alert("Grocery List:\n"+
+groceryList.join("\n"));
+alert("Grocery List(Reversed):\n"+
+groceryListReverse.join("\n"));
+  
